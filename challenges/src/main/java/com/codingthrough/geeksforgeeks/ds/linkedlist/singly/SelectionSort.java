4e5b4dd470d304d766ad8ce388f2b5cf23b31487
auntie-joye <auntie.joye@gmail.com>
2015-11-29 15:46:48 -0500

    Some linked list soled problems + unit tests.

---
+++ challenges/src/main/java/com/codingthrough/geeksforgeeks/ds/linkedlist/singly/SelectionSort.java
@@ -0,0 +1,29 @@
+package com.codingthrough.geeksforgeeks.ds.linkedlist.singly;
+
+/**
+ * Selects the specified linked list using selection sort algorithm.
+ */
+public class SelectionSort {
+    public static <T extends Comparable<T>> Node<T> sort(Node<T> head) {
+        Node<T> dummy = new Node<>(null, head);
+        Node<T> sorted = dummy;
+        while (sorted.next != null) {
+            Node<T> preMin = sorted;
+            Node<T> cur = sorted.next;
+            while (cur.next != null) {
+                if (cur.next.data.compareTo(preMin.next.data) < 0) {
+                    preMin = cur;
+                }
+                cur = cur.next;
+            }
+
+            Node<T> min = preMin.next;
+            preMin.next = preMin.next.next;
+            min.next = sorted.next;
+            sorted.next = min;
+
+            sorted = sorted.next;
+        }
+        return dummy.next;
+    }
+}
---
+++ challenges/src/test/java/com/codingthrough/geeksforgeeks/ds/linkedlist/singly/SelectionSortTest.java
@@ -0,0 +1,60 @@
+package com.codingthrough.geeksforgeeks.ds.linkedlist.singly;
+
+import org.junit.Test;
+
+import static com.codingthrough.geeksforgeeks.ds.linkedlist.singly.IsLinkedListEqual.identicalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Unit tests for {@link SelectionSort}.
+ */
+public class SelectionSortTest extends TestSupport {
+    @Test
+    public void canSortEmptyList() {
+        Node<Integer> head = null;
+        assertThat(SelectionSort.sort(head), is(nullValue()));
+    }
+
+    @Test
+    public void canSortOneElementList() {
+        Node<Integer> head = buildLinkedList(1);
+        assertThat(SelectionSort.sort(head), identicalTo(head));
+    }
+
+    @Test
+    public void canSortTwoElementsUnsortedList() {
+        Node<Integer> head = buildLinkedList(2, 1);
+        Node<Integer> expected = buildLinkedList(1, 2);
+        assertThat(SelectionSort.sort(head), identicalTo(expected));
+    }
+
+    @Test
+    public void canSortTwoElementsSortedList() {
+        Node<Integer> head = buildLinkedList(1, 2);
+        Node<Integer> expected = buildLinkedList(1, 2);
+        assertThat(SelectionSort.sort(head), identicalTo(expected));
+    }
+
+    @Test
+    public void canSortSeveralElementsUnsortedList() {
+        Node<Integer> head = buildLinkedList(4, 1, 5, 2, 3);
+        Node<Integer> expected = buildLinkedList(1, 2, 3, 4, 5);
+        assertThat(SelectionSort.sort(head), identicalTo(expected));
+    }
+
+    @Test
+    public void canSortSeveralElementsSortedList() {
+        Node<Integer> head = buildLinkedList(1, 2, 3, 4, 5);
+        Node<Integer> expected = buildLinkedList(1, 2, 3, 4, 5);
+        assertThat(SelectionSort.sort(head), identicalTo(expected));
+    }
+
+    @Test
+    public void canSortSeveralElementsReverseSortedList() {
+        Node<Integer> head = buildLinkedList(5, 4, 3, 2, 1);
+        Node<Integer> expected = buildLinkedList(1, 2, 3, 4, 5);
+        assertThat(SelectionSort.sort(head), identicalTo(expected));
+    }
+}
